908ec7d49ff65037171562e7cc7664374a58b7d3
Natalija Germek <n.germek@icloud.com>
2023-01-05 01:27:59 -0800

    added pk to CharField, troubleshooting server

--- snacks/models.py
+++ snacks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Snack(models.Model):
-    name = models.CharField(primary_key=True, max_length=64)
+    name = models.CharField(max_length=64, primary_key=True)
     purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     description = models.TextField()
 
